b10b0ec84f7ee36aa046559e3f5b1c29da888bb9
Vinicius Yukio Ishikawa <viniyishikawa@gmail.com>
2025-01-19 12:56:35 -0300

    Adicionado animação de exercício concluído

--- src/app/components/card-exercicio/card-exercicio.component.ts
+++ src/app/components/card-exercicio/card-exercicio.component.ts
@@ -3,6 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Exercicio } from '../../shared/models/exercicio.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'card-exercicio',
@@ -10,11 +11,17 @@ import { Exercicio } from '../../shared/models/exercicio.model';
   imports: [
     MatCardModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    CommonModule
   ],
   templateUrl: './card-exercicio.component.html',
   styleUrl: './card-exercicio.component.scss'
 })
 export class CardExercicioComponent {
   @Input() exercicio!: Exercicio;
+  isOverlayVisible: boolean = false;
+
+  toggleOverlay(): void {
+    this.isOverlayVisible = !this.isOverlayVisible;
+  }
 }
